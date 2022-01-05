6412b0807564f43639b250f5a0c1be4492ebd34d
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-05 15:29:36 +0900

    if를 사용한 조건부 렌더링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = '리액트';
+  let rtn='';
+  if(name === '리액트'){
+    rtn=(<h1>리액트안녕</h1>);
+  }
+  else{
+    rtn=(<h2>잘 작동하냐</h2>)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {rtn}
     </div>
   );
 }
