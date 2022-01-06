be20176a54585c2f5d18def8cda5936f12b68645
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-06 23:04:23 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,7 @@
 function App() {
   const name = '리액트';
-  const style = {
-    backgroundcolor: 'black',
-    color: 'aqua',
-    fontsize : '48px',
-    fontweight : 'bold',
-    padding: 16
-  };
   return (
-    <div style={style}>{name}
+    <div className="react">{name}
     </div>
   );
 }
