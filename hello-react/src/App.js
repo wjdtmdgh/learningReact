6cca1f41a61928a42c3909874881757cca7c6e33
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-06 23:22:23 +0900

    클래스형 컴포넌트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,9 @@
-function App() {
-  const name = '리액트';
-  return (
-    <>
-    {/* 주석은 이렇게 작성하여야 합니다. */}
-    <div className="react">{name}</div>
-    //하지만 이런 주석이나
-    /*이런 주석은 페이지에 그대로 나타나게 됩니다. */
-    <input />
-    </>
-  );
+import { Component }from 'react';
+class App extends Component{
+  render(){
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
