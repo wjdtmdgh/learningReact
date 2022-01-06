07efa804969f28f9c8bedf63cc325f2712feb9bb
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-06 23:10:58 +0900

    주석

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,10 @@ function App() {
   const name = '리액트';
   return (
     <>
+    {/* 주석은 이렇게 작성하여야 합니다. */}
     <div className="react">{name}</div>
+    //하지만 이런 주석이나
+    /*이런 주석은 페이지에 그대로 나타나게 됩니다. */
     <input />
     </>
   );
