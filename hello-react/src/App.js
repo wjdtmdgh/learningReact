27e18f945e3c6fb0206bd64240d98daa39706e2a
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-06 23:40:56 +0900

    모듈 내보내기 및 불러오기(MyComponent)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,6 @@
-import { Component }from 'react';
-class App extends Component{
-  render(){
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
+import  MyComponent from './MyComponent';
+const App =()=>{
+  return<MyComponent/>;
+};
 
 export default App;
