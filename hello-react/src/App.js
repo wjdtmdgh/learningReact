86b8465d0305c8c23cc99fdc97f860d795cbbcdd
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-02-22 23:31:01 +0900

    클래스형 컴포넌트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
-import  MyComponent from './MyComponent';
-const App =()=>{
-  return(
-    <div>
-      <MyComponent MyComponent name="React">리액트</MyComponent>
-    </div>
-  );
-};
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
+}
 
 export default App;
