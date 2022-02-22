08910385986bd32446190001765d2e3611d2b786
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-02-22 23:51:59 +0900

    props사용 값 지정하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,18 @@
 import { Component } from "react";
 import "./App.css";
-
+import MyComponent from "./MyComponent";
 class App extends Component {
   render() {
     const name = "react";
-    return <div className="react">{name}</div>;
+    return (
+      <div className="react">
+        {name}
+        <MyComponent name="react2" />
+        <MyComponent name="react3" />
+        <MyComponent name="react4" />
+        <MyComponent name="react5" />
+      </div>
+    );
   }
 }
 
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,4 @@
+function MyComponent(props) {
+  return <div>나의 멋진 컴포넌트 {props.name}</div>;
+}
+export default MyComponent;
