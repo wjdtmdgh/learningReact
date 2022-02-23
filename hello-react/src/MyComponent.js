e9ff62e109811aaf2cfc468ec46f240a091917ac
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-02-23 21:56:51 +0900

    propTypes를 통한 props 검증

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,9 +1,12 @@
-function MyComponent(props) {
-  const { hello, children } = props;
+import PropTypes from "prop-types";
+function MyComponent({ hello, children }) {
   return (
     <div>
       나의 {hello} 멋진 {children} 컴포넌트
     </div>
   );
 }
+MyComponent.propTypes = {
+  hello: PropTypes.string,
+};
 export default MyComponent;
