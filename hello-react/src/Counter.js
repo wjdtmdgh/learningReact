c74d889fb87de88bf2fb6d3fd0f65ae7a2a1fb81
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-02-23 22:47:11 +0900

    this.setState가 끝난 후 특정 작업

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,20 +1,22 @@
 import { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixednumber: 0,
+  };
   render() {
-    const { number } = this.state;
+    const { number, fixednumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>{fixednumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              console.log("방금 setState 호출");
+              console.log(this.state);
+            });
           }}
         >
           +1
