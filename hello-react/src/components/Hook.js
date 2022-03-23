2bd73134cfc58ecafce3ee893faa59247268e3be
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-03-23 21:39:06 +0900

    useReducer

--- hello-react/src/components/Hook.js
+++ hello-react/src/components/Hook.js
@@ -1,31 +1,25 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 
+function reducer(state, action) {
+  console.log(1);
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
 const Hook = () => {
-  const [name, setName] = useState(0);
-  const [nickname, setNickname] = useState(0);
-  useEffect(() => {
-    console.log(name);
-  }, [name]);
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
   return (
     <div>
-      <input value={name} onChange={onChangeName} />
-      <input value={nickname} onChange={onChangeNickname} />
-      <div>
-        <div>
-          <b>이름:</b>
-          {name}
-        </div>
-        <div>
-          <b>닉네임:</b>
-          {nickname}
-        </div>
-      </div>
+      <p>
+        현재 카운터값은<b>{state.value}</b>입니다
+      </p>
+      <button onClick={dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={dispatch({ type: "DECREMENT" })}>+1</button>
     </div>
   );
 };
