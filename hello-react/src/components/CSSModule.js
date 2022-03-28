3faf8c4f31fd9d02ffd6dc674428b96892312b99
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-03-28 22:59:32 +0900

    classNames

--- hello-react/src/components/CSSModule.js
+++ hello-react/src/components/CSSModule.js
@@ -1,9 +1,10 @@
 import styles from "./CSSModule.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 const CSSModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
-      안녕하세요 저는 CSS Module!
-    </div>
+    <div className={cx("wrapper", "inverted")}>안녕하세요 저는 CSS Module!</div>
   );
 };
 export default CSSModule;
