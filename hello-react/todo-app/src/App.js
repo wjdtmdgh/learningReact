64e162d748ea79af977b4b60ed01f622efd8e9e5
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-03-29 21:23:47 +0900

    TodoList TodoListItem 구현

--- hello-react/todo-app/src/App.js
+++ hello-react/todo-app/src/App.js
@@ -1,9 +1,11 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
