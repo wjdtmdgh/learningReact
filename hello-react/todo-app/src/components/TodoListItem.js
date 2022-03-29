f2d71cecce61174bb3fca7b573ca980afec43e33
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-03-29 21:40:38 +0900

    기능구현하기 Todo

--- hello-react/todo-app/src/components/TodoListItem.js
+++ hello-react/todo-app/src/components/TodoListItem.js
@@ -3,14 +3,16 @@ import {
   MdCheckBox,
   MdRemoveCircleOutline,
 } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
+      <div className={cn('checkbox', { checked })}>
         <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
